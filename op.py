e488ffff176917109818870d136f0bc8af784908
op0102 <92190792+op0102@users.noreply.github.com>
2021-10-09 05:58:36 +0530

    Initial Commit

---
+++ op.py
@@ -0,0 +1,108 @@
+# a="Omprakash Chetiyar is a Good Person"
+# print(a.count('a'))
+# i=0
+# while i in range(25):
+#     print(i)
+#     i=i+1
+# l=[11,5,8,9,6,1]
+# for item in l:
+#     if item==8:      
+#        pass
+   
+
+# else:
+# #     print("DONE")
+# def percent(marks):
+#     return (marks[0]+ marks[1]+ marks[2]+ marks[3]+ marks[4])/4
+
+# marks1=[25,85,65,35,65]
+# percent1=percent(marks1)
+# print(percent1)
+# def factorial(n):
+#     if n==1 or n==0:
+#         return 1
+#     else:
+#         return n*factorial(n-1)
+# x=int(input("Enter Your Number :"))
+# y=factorial(x)
+# print(y)
+# def hcf(a,b):
+#     if b==0:
+#         return a
+#     else:
+#         return hcf(b,a%b)
+# om=hcf(8,52)
+# print(om)# def lcm(a,b):
+#     lcm.multiple=lcm.multiple+b
+#     if((lcm.multiple%a==0) and (lcm.multiple%b==0)):
+#         return lcm.multiple
+#     else:
+#         lcm(a,b)
+#         return  lcm.multiple
+# lcm.multiple=0
+# x=lcm(2,3)
+# print(x)
+# a=54?
+# def func1():
+#     global a
+#     print(a)
+#     a=8
+#     print(a)
+# func1()
+# print(a)
+# l=[25,63,6,2,55,87,59,8,25]
+# sum=lambda a:a*(a-3)
+# print(sum(1))
+
+# l=
+# sent=" or ".join(l)
+# from functools import reduce
+                            
+# def add(x,y):
+#     return x+y
+# list=[2,3,5,6]
+# print(reduce(add,list))
+                                                                            
+
+# def swap(list):
+#     size=len(list)
+#     temp=list[0]
+#     list[0]=list[size-1]
+#     list[size-1]=temp
+
+# l= [1,5,3,8,5,"om","baba","sita",12222]
+# a=swap(l)
+# print(l)
+
+# b=5
+# a=4
+# a,b=b,a
+# print(a)
+# print(b)
+
+# def swap(x):
+#    y=len(x)
+#    first=x[0]
+#    x[0]=x[y-1]
+#    x[y-1]=first
+#    return x
+
+# l= ["Omprakash",5,3,8,5,"om","baba","sita",12222]
+# a= swap(l)
+# print(a)
+    
+# def swap(x):
+#     x[0],x[-1]=x[-1],x[0]
+#     return x
+# l= ["Omprakash",5,3,8,5,"om","baba","sita",12222]
+# a= swap(l)
+# print(a)
+
+A= "O M P R A K A S H"
+print
+
+
+
+
+                                                                               
+                                                                                                                                                                                                        
